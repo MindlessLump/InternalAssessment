6579451f1980a7952ca1daefe303787a2236195b
Erik Martin <ewok.attok@gmail.com>
2015-02-11 10:10:25 -0700

    Formatting and extra results fields

--- src/javaApplet/JFrameRunner.java
+++ src/javaApplet/JFrameRunner.java
@@ -21,9 +21,13 @@ public class JFrameRunner extends JFrame implements ActionListener {
 	JTextField modifier100, modifier20, modifier12, modifier10, modifier8, modifier6, modifier4, modifier3;
 	//Roll: Button that the user clicks to roll that die type, with the given number of dice and total modifier.
 	JButton roll100, roll20, roll12, roll10, roll8, roll6, roll4, roll3;
+	
+	//Results: Text fields with individual results for the most recent die roll for each die type.
+	JTextField res100, res20, res12, res10, res8, res6, res4, res3;
 	//Results: Scrolling text field for the dice results.
 	JTextArea results;
 	JScrollPane scroll;
+	
 	//Clear and reset: Clears fields and output.
 	JButton clear, reset;
 	
@@ -34,6 +38,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 	JLabel die100, die20, die12, die10, die8, die6, die4, die3;
 	JLabel mod100, mod20, mod12, mod10, mod8, mod6, mod4, mod3;
 	JLabel img100, img20, img12, img10, img8, img6, img4, img3;
+	JLabel imgTitle, numberTitle, modifierTitle, resTitle;
 	
 	//Various JPanels to help with layout
 	JPanel window, inputField, outputField;
@@ -84,6 +89,26 @@ public class JFrameRunner extends JFrame implements ActionListener {
 		roll4.addActionListener(this);
 		roll3.addActionListener(this);
 		
+		res100 = new JTextField("");
+		res20 = new JTextField("");
+		res12 = new JTextField("");
+		res10 = new JTextField("");
+		res8 = new JTextField("");
+		res6 = new JTextField("");
+		res4 = new JTextField("");
+		res3 = new JTextField("");
+		
+		res100.setEditable(false);
+		res20.setEditable(false);
+		res12.setEditable(false);
+		res10.setEditable(false);
+		res8.setEditable(false);
+		res6.setEditable(false);
+		res4.setEditable(false);
+		res3.setEditable(false);
+		
+		res100.setPreferredSize(new Dimension(48, 24));
+		
 		results = new JTextArea("The results will appear in this text field,\nwith the newest rolls on the bottom.\n--------------------");
 		results.setEditable(false);
 		scroll = new JScrollPane (results, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -122,6 +147,11 @@ public class JFrameRunner extends JFrame implements ActionListener {
 		img6 = new JLabel();
 		img4 = new JLabel();
 		img3 = new JLabel();
+		
+		imgTitle = new JLabel("Die");
+		numberTitle = new JLabel("Number");
+		modifierTitle = new JLabel("Modifier");
+		resTitle = new JLabel("Results");
 		
 		ImageIcon d100 = new ImageIcon("src/d100.jpg");
 		if(d100 != null) {
@@ -166,11 +196,8 @@ public class JFrameRunner extends JFrame implements ActionListener {
 		
 		//Add the input and output objects to their respective containers (JPanel)
 		window = new JPanel();
-		window.setSize(800, 400);
 		inputField = new JPanel();
-		inputField.setSize(600, 400);
 		outputField = new JPanel();
-		outputField.setSize(200, 400);
 		
 		//Define the applet layout
 		doLayoutWork();
@@ -193,6 +220,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier100.getText());
 			String result = number100.getText() + "d100 + " + modifier100.getText() + "\n= " + dice + " + " + modifier100.getText() + "\n= " + total + ".";
+			res100.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll20) { //If the button for rolling the d20 has been pressed.
@@ -202,6 +230,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier20.getText());
 			String result = number20.getText() + "d20 + " + modifier20.getText() + "\n= " + dice + " + " + modifier20.getText() + "\n= " + total + ".";
+			res20.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll12) { //If the button for rolling the d12 has been pressed.
@@ -211,6 +240,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier12.getText());
 			String result = number12.getText() + "d12 + " + modifier12.getText() + "\n= " + dice + " + " + modifier12.getText() + "\n= " + total + ".";
+			res12.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll10) { //If the button for rolling the d10 has been pressed.
@@ -220,6 +250,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier10.getText());
 			String result = number10.getText() + "d10 + " + modifier10.getText() + "\n= " + dice + " + " + modifier10.getText() + "\n= " + total + ".";
+			res10.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll8) { //If the button for rolling the d8 has been pressed.
@@ -229,6 +260,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier8.getText());
 			String result = number8.getText() + "d8 + " + modifier8.getText() + "\n= " + dice + " + " + modifier8.getText() + "\n= " + total + ".";
+			res8.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll6) { //If the button for rolling the d6 has been pressed.
@@ -238,6 +270,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier6.getText());
 			String result = number6.getText() + "d6 + " + modifier6.getText() + "\n= " + dice + " + " + modifier6.getText() + "\n= " + total + ".";
+			res6.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll4) { //If the button for rolling the d4 has been pressed.
@@ -247,6 +280,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier4.getText());
 			String result = number4.getText() + "d4 + " + modifier4.getText() + "\n= " + dice + " + " + modifier4.getText() + "\n= " + total + ".";
+			res4.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == roll3) { //If the button for rolling the d3 has been pressed.
@@ -256,6 +290,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			}
 			int total = dice + Integer.parseInt(modifier3.getText());
 			String result = number3.getText() + "d3 + " + modifier3.getText() + "\n= " + dice + " + " + modifier3.getText() + "\n= " + total + ".";
+			res3.setText("" + total);
 			results.setText(results.getText() + "\n\n" + result);
 		}
 		if(e.getSource() == clear) { //If the button for clearing the output field has been pressed.
@@ -279,6 +314,15 @@ public class JFrameRunner extends JFrame implements ActionListener {
 			modifier6.setText("0");
 			modifier4.setText("0");
 			modifier3.setText("0");
+			
+			res100.setText("");
+			res20.setText("");
+			res12.setText("");
+			res10.setText("");
+			res8.setText("");
+			res6.setText("");
+			res4.setText("");
+			res3.setText("");
 		}
 	}
 	
@@ -307,6 +351,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 		inputLayout.setHorizontalGroup(
 				inputLayout.createSequentialGroup()
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+							.addComponent(imgTitle)
 							.addComponent(img100)
 							.addComponent(img20)
 							.addComponent(img12)
@@ -316,6 +361,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 							.addComponent(img4)
 							.addComponent(img3))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+							.addComponent(numberTitle)
 							.addComponent(number100)
 							.addComponent(number20)
 							.addComponent(number12)
@@ -343,6 +389,7 @@ public class JFrameRunner extends JFrame implements ActionListener {
 							.addComponent(mod4)
 							.addComponent(mod3))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+							.addComponent(modifierTitle)
 							.addComponent(modifier100)
 							.addComponent(modifier20)
 							.addComponent(modifier12)
@@ -359,68 +406,89 @@ public class JFrameRunner extends JFrame implements ActionListener {
 							.addComponent(roll8)
 							.addComponent(roll6)
 							.addComponent(roll4)
-							.addComponent(roll3)
-							.addComponent(reset))
+							.addComponent(roll3))
+					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+							.addComponent(resTitle)
+							.addComponent(res100)
+							.addComponent(res20)
+							.addComponent(res12)
+							.addComponent(res10)
+							.addComponent(res8)
+							.addComponent(res6)
+							.addComponent(res4)
+							.addComponent(res3))
 		);
 		inputLayout.setVerticalGroup(
 				inputLayout.createSequentialGroup()
+					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+							.addComponent(imgTitle)
+							.addComponent(numberTitle)
+							.addComponent(modifierTitle)
+							.addComponent(resTitle))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img100)
 							.addComponent(number100)
 							.addComponent(die100)
 							.addComponent(mod100)
 							.addComponent(modifier100)
-							.addComponent(roll100))
+							.addComponent(roll100)
+							.addComponent(res100))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img20)
 							.addComponent(number20)
 							.addComponent(die20)
 							.addComponent(mod20)
 							.addComponent(modifier20)
-							.addComponent(roll20))
+							.addComponent(roll20)
+							.addComponent(res20))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img12)
 							.addComponent(number12)
 							.addComponent(die12)
 							.addComponent(mod12)
 							.addComponent(modifier12)
-							.addComponent(roll12))
+							.addComponent(roll12)
+							.addComponent(res12))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img10)
 							.addComponent(number10)
 							.addComponent(die10)
 							.addComponent(mod10)
 							.addComponent(modifier10)
-							.addComponent(roll10))
+							.addComponent(roll10)
+							.addComponent(res10))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img8)
 							.addComponent(number8)
 							.addComponent(die8)
 							.addComponent(mod8)
 							.addComponent(modifier8)
-							.addComponent(roll8))
+							.addComponent(roll8)
+							.addComponent(res8))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img6)
 							.addComponent(number6)
 							.addComponent(die6)
 							.addComponent(mod6)
 							.addComponent(modifier6)
-							.addComponent(roll6))
+							.addComponent(roll6)
+							.addComponent(res6))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img4)
 							.addComponent(number4)
 							.addComponent(die4)
 							.addComponent(mod4)
 							.addComponent(modifier4)
-							.addComponent(roll4))
+							.addComponent(roll4)
+							.addComponent(res4))
 					.addGroup(inputLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
 							.addComponent(img3)
 							.addComponent(number3)
 							.addComponent(die3)
 							.addComponent(mod3)
 							.addComponent(modifier3)
-							.addComponent(roll3))
-					.addComponent(reset)
+							.addComponent(roll3)
+							.addComponent(res3))
 		);
 		
 		//Organize the output layout
@@ -429,15 +497,19 @@ public class JFrameRunner extends JFrame implements ActionListener {
 		outputLayout.setAutoCreateGaps(true);
 		outputLayout.setAutoCreateContainerGaps(true);
 		outputLayout.setHorizontalGroup(
-				outputLayout.createSequentialGroup()
-					.addGroup(outputLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-							.addComponent(scroll)
+				outputLayout.createParallelGroup()
+					.addComponent(scroll)
+					.addGroup(outputLayout.createSequentialGroup()
+							.addComponent(reset)
+							.addContainerGap(82, 82)
 							.addComponent(clear))
 		);
 		outputLayout.setVerticalGroup(
 				outputLayout.createSequentialGroup()
 					.addComponent(scroll)
-					.addComponent(clear)
+					.addGroup(outputLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+							.addComponent(reset)
+							.addComponent(clear))
 		);
 	}
 }
